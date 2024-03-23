cc7b204f8d7711ab06102715574a599e07576243
lucileneD <lgdeweese@gmail.com>
2024-03-23 06:59:18 -0400

    week 05 assigments

---
+++ scripts/links.js
@@ -0,0 +1,29 @@
+/*************************************
+* Dynamic Links Using JSON Functions *
+*************************************/
+const baseurl = 'https://github.com/lucileneD/wdd230';
+const linksurl = 'data/links.json';
+const linkMenu = document.querySelector('#courseLinks');
+
+async function getLinkData() {
+    const response = await fetch(linksurl);
+    const data = await response.json();
+    displayLinks(data.weeks); 
+}
+
+const displayLinks = (weeks) => {
+    weeks.forEach((week) => {
+        let assignment = document.createElement('li');
+		assignment.textContent = `Week ${week.week}: `;
+		week.links.forEach((link) => {
+			let individual = document.createElement('a');
+			individual.setAttribute('href', `${link.url}`);
+			individual.setAttribute('target', '_blank');
+			individual.textContent = `${link.title} `;
+			assignment.appendChild(individual);
+		});
+        linkMenu.appendChild(assignment);
+    });
+};
+
+getLinkData();
