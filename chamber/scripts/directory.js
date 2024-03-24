ec50d7c36b5bbe8adf073dca08d4aa28fbcb1d0e
lucileneD <lgdeweese@gmail.com>
2024-03-24 19:38:07 -0400

    directory page

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,57 @@
+/*************************************
+* Dynamic Links Using JSON Functions *
+*************************************/
+
+const listurl = 'data/members.json'
+const memberList = document.querySelector('#content');
+
+async function getLinkData() {
+    const response = await fetch(listurl);
+    const data = await response.json();
+	displayLinks(data.members); 
+}
+
+const displayLinks = (members) => {
+    members.forEach((member) => {
+        let card = document.createElement('div');
+		let pic = document.createElement('img');
+		let location = document.createElement('p');
+		let call = document.createElement('p');
+		let url = document.createElement('a');
+
+        card.setAttribute('class', 'memberCard')
+		pic.setAttribute('src', member.pic);
+        pic.setAttribute('alt', `Image of ${member.name} logo`);
+        pic.setAttribute('loading', 'lazy');
+        pic.setAttribute('max-width', '200');
+        pic.setAttribute('height', 'auto');
+		location.textContent = `${member.address}`;
+		call.textContent = `${member.phone}`;
+		url.setAttribute('href', `${member.web}`);
+		url.setAttribute('target', '_blank');
+		url.textContent = `${member.name}`;
+
+
+        card.appendChild(pic);
+        card.appendChild(location);
+        card.appendChild(call);
+        card.appendChild(url);
+        memberList.appendChild(card);
+    });
+}
+
+getLinkData();
+
+
+/**************************
+* Grid/List layout toggle *
+**************************/
+
+const gridbutton = document.querySelector("#grid");
+const display = document.querySelector("#content");
+
+gridbutton.addEventListener("click", () => {
+	// example using arrow function
+	display.classList.toggle("grid");
+	display.classList.toggle("list");
+});
